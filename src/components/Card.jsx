c6017fd6044782ba1e9080a0143be269f3b7b4a1
Eden Castillo <ecastilloasport@gmail.com>
2022-03-19 21:55:47 -0500

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Card() {
+  return (
+      <div className="card">
+        <h1>Question X</h1>
+        <p>This is question number x</p>
+      </div>
+  );
+}
+
+export default Card;
