a9977fb344f45b4d9dea2a9a59932b9340013eb9
Eden Castillo <ecastilloasport@gmail.com>
2022-03-21 19:10:50 -0500

    [Fix] Improved styles and css clean up and refactor. Added new neumorphism theme.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,18 +5,12 @@ function Card(Props) {
 
   return (
     <div className="flexbox-item">
-      <button type="button" className="answer-button-card" active="true">
-      {questions[Props.id].answer1}
-      </button>
-      <button className="answer-button-card" active="true">
-      {questions[Props.id].answer2}
-      </button>
-      <button className="answer-button-card" active="true">
-      {questions[Props.id].answer3}
-      </button>
-      <button className="answer-button-card" active="true">
-      {questions[Props.id].answer4}
-      </button>
+      <p id="checkbox-button">
+        <label><input type="radio" name="answer" value="1" /><span className="answer-button-card">{questions[Props.id].answer1}</span></label>
+        <label><input type="radio" name="answer" value="1" /><span>{questions[Props.id].answer2}</span></label>
+        <label><input type="radio" name="answer" value="1" /><span>{questions[Props.id].answer3}</span></label>
+        <label><input type="radio" name="answer" value="1" /><span>{questions[Props.id].answer4}</span></label>
+      </p>
     </div>
   );
 }
--- src/components/Maincard.jsx
+++ src/components/Maincard.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Maincard(Props) {
   return (
     <div className="flexbox-item">
-    <form action="">
-      <div className="card">
-        <h1>Are you an introvert or and extrovert?</h1>
-        <h3> {Props.question}</h3>
-      </div>
-      <div className="btn-p">
-        <input className="btn" type="submit" value="Next >" />
-      </div>
-      <p className="page-count">{Props.id} of 6</p>
-    </form>
+      <form action="">
+        <div className="card">
+          <h1>Are you an introvert or and extrovert?</h1>
+          <h3> {Props.question}</h3>
+        </div>
+        <div className="btn-p">
+          <input className="btn" type="submit" value="Next >" />
+        </div>
+        <p className="page-count">{Props.id} of 6</p>
+      </form>
     </div>
   );
 }
