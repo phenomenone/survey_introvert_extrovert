6a5283cd2476c55f9de83202bcea484463b1b173
Eden Castillo <ecastilloasport@gmail.com>
2022-03-23 17:32:53 -0500

    [Fix] Fixed over all design to make it reusable.  Ie. Added usastates to ccontrol counts and selection.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useState, useRef } from "react";
 // import Header from "./Header";
 import Footer from "./Footer";
 import Card from "./Card";
@@ -6,23 +6,42 @@ import questions from "../questions";
 import Maincard from "./Maincard";
 
 function App() {
+  const [isSelected, setIsSelected] = useState(false);
 
-  let questionNumber = 0;
+  const [count, setCount ] = useState(0);
 
+  console.log(count);
   return (
-    <div>
-      <div className="flexbox-container">
-          <Maincard 
-          question={questions[questionNumber].question} 
-          id={questions[questionNumber].id}
-          />
-          <Card id={questions[questionNumber].id}/>
-      </div>
-
-    <Footer />
-    </div>
-    
-  )
+    <Fragment>
+      <form>
+        <div className="flexbox-container">
+            <div className="flexbox-item">
+              <div className="card">
+                <h1>Are you an introvert or and extrovert?</h1>
+                <h3> {questions[count].question}</h3>
+              </div>
+              <div className="btn-p">
+                { isSelected 
+                  ? <input onClick={() => {setCount(count + 1); }} className={isSelected ? "btn" : "btn-hide"} type="button" value="Next >" /> 
+                  : <input onClick={() => {alert("Please, select and option first.")}} className={isSelected ? "btn" : "btn-hide"} type="submit" value="Next >" />
+                }
+                  
+              </div>
+              <p className="page-count">{questions[count].id} of 6</p> 
+            </div>
+            <div className="flexbox-item">
+              <div id="checkbox-button">
+                <label><input onClick={()=> setIsSelected(true)} type="radio" name="answer" value="A" /><span className="glow">{questions[count].answer1}</span></label>
+                <label><input onClick={()=> setIsSelected(true)} type="radio" name="answer" value="B" /><span className="glow">{questions[count].answer2}</span></label>
+                <label><input onClick={()=> setIsSelected(true)} type="radio" name="answer" value="C" /><span className="glow">{questions[count].answer3}</span></label>
+                <label><input onClick={()=> setIsSelected(true)} type="radio" name="answer" value="D" /><span className="glow">{questions[count].answer4}</span></label>
+              </div>
+            </div>
+        </div>
+      </form>
+      <Footer /> 
+    </Fragment>
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,19 @@
-import React from "react";
+import React, { useState} from "react";
 import questions from "../questions";
 
 function Card(Props) {
- 
 
-  let onOnclickHandler = (e) => {
-    console.log(e.target.value); 
-  };
+  const [isSelected, setIsSelected] = useState(false);
+
+  console.log(isSelected);
 
   return (
     <div className="flexbox-item">
       <p id="checkbox-button">
-        <label><input onClick={onOnclickHandler} type="radio" name="answer" value="1" /><span className="glow">{questions[Props.id].answer1}</span></label>
-        <label><input onClick={onOnclickHandler} type="radio" name="answer" value="2" /><span className="glow">{questions[Props.id].answer2}</span></label>
-        <label><input onClick={onOnclickHandler} type="radio" name="answer" value="3" /><span className="glow">{questions[Props.id].answer3}</span></label>
-        <label><input onClick={onOnclickHandler} type="radio" name="answer" value="4" /><span className="glow">{questions[Props.id].answer4}</span></label>
+        <label><input onClick={()=> setIsSelected(true)} type="radio" name="answer" value="A" /><span className="glow">{questions[Props.id].answer1}</span></label>
+        <label><input onClick={((e) => {console.log(e.target.value); })} type="radio" name="answer" value="B" /><span className="glow">{questions[Props.id].answer2}</span></label>
+        <label><input onClick={((e) => {console.log(e.target.value); })} type="radio" name="answer" value="C" /><span className="glow">{questions[Props.id].answer3}</span></label>
+        <label><input onClick={((e) => {console.log(e.target.value); })} type="radio" name="answer" value="D" /><span className="glow">{questions[Props.id].answer4}</span></label>
       </p>
     </div>
   );
