dacb9ce061e04b4b096bfce2ecda5b857ae6a247
Eden Castillo <ecastilloasport@gmail.com>
2022-03-22 23:10:33 -0500

    [Add]Improve css styling and effects. Create basic logic to geet the correct components rendered.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import questions from "../questions";
 
 function Card(Props) {
+ 
+
+  let onOnclickHandler = (e) => {
+    console.log(e.target.value); 
+  };
 
   return (
     <div className="flexbox-item">
       <p id="checkbox-button">
-        <label><input type="radio" name="answer" value="1" /><span className="answer-button-card">{questions[Props.id].answer1}</span></label>
-        <label><input type="radio" name="answer" value="1" /><span>{questions[Props.id].answer2}</span></label>
-        <label><input type="radio" name="answer" value="1" /><span>{questions[Props.id].answer3}</span></label>
-        <label><input type="radio" name="answer" value="1" /><span>{questions[Props.id].answer4}</span></label>
+        <label><input onClick={onOnclickHandler} type="radio" name="answer" value="1" /><span className="glow">{questions[Props.id].answer1}</span></label>
+        <label><input onClick={onOnclickHandler} type="radio" name="answer" value="2" /><span className="glow">{questions[Props.id].answer2}</span></label>
+        <label><input onClick={onOnclickHandler} type="radio" name="answer" value="3" /><span className="glow">{questions[Props.id].answer3}</span></label>
+        <label><input onClick={onOnclickHandler} type="radio" name="answer" value="4" /><span className="glow">{questions[Props.id].answer4}</span></label>
       </p>
     </div>
   );
