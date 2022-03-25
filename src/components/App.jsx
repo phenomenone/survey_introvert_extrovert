fbeeaa40ed8a6c6ef7a0b96bd45517ad28ee6c64
Eden Castillo <ecastilloasport@gmail.com>
2022-03-24 23:00:10 -0500

    [Add] Imporved and added results page styling and react logic to reload page. Improved fucntions. Update correct app name in json package.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import React, { Fragment, useState } from "react";
 import Footer from "./Footer";
 import questions from "../questions";
 
+
 function App() {
 
   const [isSelected, setIsSelected] = useState(false);
@@ -15,11 +16,12 @@ function App() {
 
   return (
     <Fragment>
-      <form>
+      {count < 6
+      ?<form>
         <div className="flexbox-container">
             <div className="flexbox-item">
               <div className="card">
-                <h1>Are you an introvert or and extrovert?</h1>
+                <h1>Are you an introvert or an extrovert?</h1>
                 <h3> {questions[count].question}</h3>
                 <p className="page-count">{questions[count].id}/6</p> 
               </div>
@@ -40,6 +42,16 @@ function App() {
             </div>
         </div>
       </form>
+      :
+      <div className="result-page">
+        <h1>Congratulations!</h1>
+        <h2>Your score is {addNumber}!!!</h2>
+        <h3>You are an ...</h3>
+        <h1>{addNumber < 12? "Introvert!": "Extrovert!"}</h1>
+        <input className="result-page-btn" type="submit" value="Try it Again!" onClick={() => window.location.reload(false)}/>
+      </div>
+
+      }
       <Footer /> 
     </Fragment>
   );
